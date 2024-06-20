e30a909386a583bb60ea425ddbff3dac6737549c
weiyuchens <weiyuchens@sina.com>
2024-06-20 15:21:50 +0800

    修改数据库结构

--- database/uninews.sql
+++ database/uninews.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80400
  File Encoding         : 65001
 
- Date: 24/05/2024 15:54:31
+ Date: 20/06/2024 15:21:10
 */
 
 SET NAMES utf8mb4;
@@ -22,49 +22,72 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '文章ID',
-  `university_id` int(0) NULL DEFAULT NULL COMMENT '大学ID',
-  `title` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '文章题目',
-  `source` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '文章来源',
-  `date` date NULL DEFAULT NULL COMMENT '文章发表时间',
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '文章内容',
-  `url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '文章URL',
-  `picture` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '文章当中的照片',
-  `attachment_url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '附件URL',
-  `crawl_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '爬取时间',
-  `update_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `university_id` int(0) NULL DEFAULT NULL,
+  `crawler_task_id` int(0) NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `source` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `date` date NULL DEFAULT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `attachment_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `status` int(0) NULL DEFAULT NULL,
+  `crawler_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `crawl_time` timestamp(0) NULL DEFAULT NULL,
+  `update_time` timestamp(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_article_crawler_task`(`crawler_task_id`) USING BTREE,
   INDEX `fk_article_university`(`university_id`) USING BTREE,
-  CONSTRAINT `fk_article_university` FOREIGN KEY (`university_id`) REFERENCES `university` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  CONSTRAINT `fk_article_crawler_task` FOREIGN KEY (`crawler_task_id`) REFERENCES `crawler_task` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_article_university` FOREIGN KEY (`university_id`) REFERENCES `university` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for city
 -- ----------------------------
 DROP TABLE IF EXISTS `city`;
 CREATE TABLE `city`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '城市ID',
-  `city_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '城市名字',
-  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
-  `update_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `city_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_time` timestamp(0) NULL DEFAULT NULL,
+  `update_time` timestamp(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for crawler_task
+-- ----------------------------
+DROP TABLE IF EXISTS `crawler_task`;
+CREATE TABLE `crawler_task`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `crawler_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `assigned` tinyint(1) NULL DEFAULT 0,
+  `url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `status` int(0) NULL DEFAULT NULL,
+  `create_time` timestamp(0) NULL DEFAULT NULL,
+  `update_time` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for university
 -- ----------------------------
 DROP TABLE IF EXISTS `university`;
 CREATE TABLE `university`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '学校ID',
-  `city_id` int(0) NULL DEFAULT NULL COMMENT '城市ID',
-  `university_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '学校名字',
-  `university_website_url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '学校官方网站url',
-  `university_start_url` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '学校研究生招生网站url',
-  `create_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
-  `update_time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `city_id` int(0) NULL DEFAULT NULL,
+  `crawler_task_id` int(0) NULL DEFAULT NULL,
+  `university_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `university_website_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `university_start_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_time` timestamp(0) NULL DEFAULT NULL,
+  `update_time` timestamp(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_university_crawler_task`(`crawler_task_id`) USING BTREE,
   INDEX `fk_university_city`(`city_id`) USING BTREE,
-  CONSTRAINT `fk_university_city` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  CONSTRAINT `fk_university_city` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_university_crawler_task` FOREIGN KEY (`crawler_task_id`) REFERENCES `crawler_task` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
